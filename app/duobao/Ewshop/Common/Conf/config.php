8e63905aecb26b93a0bb4ee42853d85a5e541874
bankie <ebankie@126.com>
2017-07-19 09:43:10 +0800

    Update config.php

--- app/duobao/Ewshop/Common/Conf/config.php
+++ app/duobao/Ewshop/Common/Conf/config.php
@@ -40,9 +40,9 @@ return array (
     /* 数据库配置 */
     'DB_TYPE' => 'mysql' , // 数据库类型
     'DB_HOST' => 'localhost' , // 服务器地址
-    'DB_NAME' => 'duobao' , // 数据库名
-    'DB_USER' => 'root' , // 用户名
-    'DB_PWD' => 'ewangtx2017' ,  // 密码
+    'DB_NAME' => '' , // 数据库名
+    'DB_USER' => '' , // 用户名
+    'DB_PWD' => '' ,  // 密码
     'DB_PORT' => '3306' , // 端口
     'DB_PREFIX' => 'ewshop_' , // 数据库表前缀
     'DB_CHARSET' => 'utf8' , // 字符集
